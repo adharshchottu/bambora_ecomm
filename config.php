dcdc28291f06db0fe91af2997122a0b539a11769
Adharsh Chottu <81185662+adharshchottu@users.noreply.github.com>
2021-03-23 14:35:35 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'sql310.epizy.com');
-define('DB_USERNAME', 'epiz_25704564');
-define('DB_PASSWORD', 'Augu18101999');
-define('DB_NAME', 'epiz_25704564_ec');
+define('DB_SERVER', 'your host');
+define('DB_USERNAME', 'your username of database');
+define('DB_PASSWORD', 'your password of database');
+define('DB_NAME', 'your database name');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
