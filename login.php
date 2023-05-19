e73b7bb82780d4e9dd983f4ad20ce90de327c99b
Adharsh Chottu <81185662+adharshchottu@users.noreply.github.com>
2023-05-19 16:49:04 +0530

    Update login.php

--- login.php
+++ login.php
@@ -94,18 +94,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <meta charset="UTF-8">
     <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
- <script>
-var something = (function() {
-    var executed = false;
-    return function() {
-        if (!executed) {
-            window.open("http://gestyy.com/eyk8aI"); 
-            executed = true;
-            
-        }
-    };
-})();
-</script>
     <style type="text/css">
         body{ font: 14px sans-serif; }
         .wrapper{ width: 350px; padding: 20px; }
